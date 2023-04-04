4763502335f523331d5fd245b2b489feff881e80
Einshine <theeinshine3301@gmail.com>
2023-04-04 12:14:57 +0530

    added loader css

--- src/components/CRUD.jsx
+++ src/components/CRUD.jsx
@@ -1,10 +1,11 @@
-import React, { useContext , useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { mainData } from "../App";
 import CrudStyles from "../assets/css/Crud.css";
 import { Link } from "react-router-dom";
 
 const CRUD = () => {
   const [getUserData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true); 
   const getData = async (e) => {
     const res = await fetch("https://mongoserver-2d84.onrender.com/getdata", {
       method: "GET",
@@ -18,6 +19,7 @@ const CRUD = () => {
       console.log("Invalid data");
     } else {
       setUserData(resData.userData);
+      setLoading(false); 
       console.log("data fetched Successfully");
     }
   };
@@ -30,12 +32,15 @@ const CRUD = () => {
   }, []);
 
   const deleteuser = async (id) => {
-    const res3 = await fetch(`https://mongoserver-2d84.onrender.com/deleteuser/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const res3 = await fetch(
+      `https://mongoserver-2d84.onrender.com/deleteuser/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const deletedData = await res3.json();
 
     if (res3.status === 422 || !deletedData) {
@@ -60,65 +65,70 @@ const CRUD = () => {
             </Link>
           </div>
         </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Description</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* <tr>
-            <td>0</td>
-            <td>Test</td>
-            <td>HardCodedTest@test.com</td>
-            <td>Ignore this HardCoded Manually</td>
-            <td>
-              <Link to={`view/${"0123"}`} className="viewBtn">
-                <button className="viewBtn">View</button>
-              </Link>
+        {loading ? ( // Conditionally render loader or table
+          <div className="loader">Loading...</div>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Description</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <tr>
+              <td>0</td>
+              <td>Test</td>
+              <td>HardCodedTest@test.com</td>
+              <td>Ignore this HardCoded Manually</td>
+              <td>
+                <Link to={`view/${"0123"}`} className="viewBtn">
+                  <button className="viewBtn">View</button>
+                </Link>
 
-              <Link to={`edit/${"0123"}`} className="editBtn">
-                <button className="editBtn">Edit</button>
-              </Link>
-              <Link className="deleteBtn">
-                <button className="deleteBtn">Delete</button>
-              </Link>
-            </td>
-          </tr> */}
-            {getUserData.map((item, ind) => {
-              return (
-                <tr key={ind}>
-                  <td>{ind + 1}</td>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.description}</td>
-                  <td>
-                    <Link to={`view/${item._id}`} className="viewBtn">
-                      <button className="viewBtn">View</button>
-                    </Link>
-                    <Link to={`edit/${item._id}`} className="editBtn">
-                      <button className="editBtn">Edit</button>
-                    </Link>
-                    <Link className="deleteBtn">
-                      <button
-                        onClick={() => {
-                          deleteuser(item._id);
-                        }}
-                        className="deleteBtn"
-                      >
-                        Delete
-                      </button>
-                    </Link>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                <Link to={`edit/${"0123"}`} className="editBtn">
+                  <button className="editBtn">Edit</button>
+                </Link>
+                <Link className="deleteBtn">
+                  <button className="deleteBtn">Delete</button>
+                </Link>
+              </td>
+
+              */}
+              {getUserData.map((item, ind) => {
+                return (
+                  <tr key={ind}>
+                    <td>{ind + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <Link to={`view/${item._id}`} className="viewBtn">
+                        <button className="viewBtn">View</button>
+                      </Link>
+                      <Link to={`edit/${item._id}`} className="editBtn">
+                        <button className="editBtn">Edit</button>
+                      </Link>
+                      <Link className="deleteBtn">
+                        <button
+                          onClick={() => {
+                            deleteuser(item._id);
+                          }}
+                          className="deleteBtn"
+                        >
+                          Delete
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
     </>
   );
